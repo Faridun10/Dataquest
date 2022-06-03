9f348201dc11da8f1d1a0e9cc1b7fdc302910f86
Faridun10 <52473306+Faridun10@users.noreply.github.com>
2022-06-03 11:06:24 -0400

    Create hanoi_towers.py

---
+++ hanoi_towers.py
@@ -0,0 +1,24 @@
+# The puzzle consists of three pegs and a stack of disks of different sizes. 
+# Initially, all disks are stacked, from largest to smallest, on the first peg. 
+# The goal is to move them to the third peg in the same order. There are two rules:
+
+# 1. We can only move one disk at a time.
+# 2. We cannot put a larger disk on top of a smaller disk.
+
+def solve_hanoi(num_disks, first_peg, middle_peg, last_peg):
+    if num_disks == 1:
+        # Base case
+        print("Move the top disk from peg {} to peg {}.".format(first_peg, last_peg))
+    else:
+        # General Case
+        # Instruction 1: Add code to solve the first subproblem
+        solve_hanoi(num_disks - 1, first_peg, last_peg, middle_peg)
+        
+        # Second subproblem: Move disk num_disks from first_peg to last_peg
+        solve_hanoi(1, first_peg, middle_peg, last_peg)
+        
+        # Instruction 2: Add code to solve the third subproblem
+        solve_hanoi(num_disks -1, middle_peg, first_peg, last_peg)
+        
+        
+solve_hanoi(3, 'A', 'B', 'C')
