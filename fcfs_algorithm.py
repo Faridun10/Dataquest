04a7194ef8a041607d18797cc273db8fb2695c1d
Faridun10 <52473306+Faridun10@users.noreply.github.com>
2023-11-13 12:48:10 -0500

    Create fcfs_algorithm.py
    We're going to implement the FCFS process scheduling algorithm to calculate the start and end times of all processes in the processes DataFrame. Recall that the FCFS algorithm schedules processes on the CPU as follows:
    
    When a process arrives, if the processor is free, then assign the process to the processor. Otherwise add the process to a queue with all processes that are waiting for the processor.
    
    Once a process finishes executing, take the process at the front of the queue and assign it to the CPU.
    
    We'll calculate the start and end times of each process by simulating the FCFS algorithm. To do so, we'll keep track of the following data:
    
    The current time. This is an integer that start at zero and increases by increments of one at each simulation step.
    The total number of processes that finished executing. This value indicates when the simulation ends.
    The waiting process queue. This is the queue that will store processes that arrive when the CPU is busy.
    The ID of the current process running on the CPU. If the CPU is not running any process, this is set to None

---
+++ fcfs_algorithm.py
@@ -0,0 +1,33 @@
+cur_time = 0
+num_processes_done = 0
+wait_queue = Queue()
+cur_pid = None
+
+# Copy and paste tempalte code here
+while num_processes_done < processes.shape[0]:
+    # Check if current process finished
+    if cur_pid is not None:
+        if processes.loc[cur_pid, "Start"] + processes.loc[cur_pid, "Duration"] == cur_time:
+            # Step 1 code goes here
+            processes.loc[cur_pid, "End"] = cur_time
+            cur_pid = None
+            num_processes_done += 1
+
+    # Handle arriving processes
+    # Step 2 code goes here
+    # Get all rows from processes with an arrival time equal to cur_time
+    ready_processes =processes[processes["Arrival"] == cur_time]
+    # Add the pid of each process in ready_processes to wait_queue
+    for pid, _ in ready_processes.iterrows():
+        wait_queue.enqueue(pid)
+
+    # Assign a process to the processor
+    if cur_pid is None and len(wait_queue) > 0:
+        # Step 3 code goes here
+        # Get and remove the ID of the process at the front of the queue
+        cur_pid = wait_queue.dequeue()
+        processes.loc[cur_pid, "Start"] = cur_time
+
+    cur_time += 1
+    
+print(processes.head())
